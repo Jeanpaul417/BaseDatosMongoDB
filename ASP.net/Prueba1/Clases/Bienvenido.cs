21fe95fbafb3d785b20f3b80d2993fc3a2c15f0a
Juan Pablo Giraldo <120842138+Jeanpaul417@users.noreply.github.com>
2024-03-19 11:28:40 -0500

    Delete ASP.net/Prueba1 directory

--- ASP.net/Prueba1/Clases/Bienvenido.cs
+++
@@ -1,9 +0,0 @@
-namespace Clases {
-    public class Bienvenido{
-
-        
-        public string Nombre(string name, string lastname){
-            return $"Welcome {name} {lastname}";
-        }
-    }
-}
