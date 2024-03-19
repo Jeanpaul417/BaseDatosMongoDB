719169d274c2931dc89ac19f213623110a24638b
Juan Pablo Giraldo <120842138+Jeanpaul417@users.noreply.github.com>
2024-03-18 21:29:29 -0500

    Add files via upload

---
+++ ASP.net/Prueba1/Clases/Bienvenido.cs
@@ -0,0 +1,9 @@
+namespace Clases {
+    public class Bienvenido{
+
+        
+        public string Nombre(string name, string lastname){
+            return $"Welcome {name} {lastname}";
+        }
+    }
+}
--- ASP.net/Prueba1/Program.cs
+++ ASP.net/Prueba1/Program.cs
@@ -5,6 +5,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/lo-mejor", () => "Pepe es lo mejor que hay en el mundo mundial es mejor de todos :3");
+
+
 app.MapGet("calculadora/suma", () => {
     Calculadora calculadora = new Calculadora();
     return calculadora.Suma(1, 2);
@@ -24,5 +27,8 @@ app.MapGet("/calculadora/divi", () => {
     Calculadora calculadora = new Calculadora();
     return calculadora.Division(1, 2);
 });
+
+
+
 
 app.Run();
